a219a575fb231048d7be838bc13a53a002b438cd
Bsitota <biruk.sitota@go.shoreline.edu>
2025-05-21 11:31:22 -0700

    get method for tag

--- HTMLManager.java
+++ HTMLManager.java
@@ -2,5 +2,17 @@ import java.util.*;
 
 public class HTMLManager {
   private Queue<HTMLTag> tags;
+  public HTMLManager(Queue<HTMLTag> html){
+     tags = new LinkedList<>();
+     if(html == null){
+       throw new IllegalArgumentException();
+     } 
+     for(HTMLTag tag : html) {
+      tags.add(tag);
+     }
+  }
+  public Queue<HTMLTag> getTags(){
+    return tags;
+  }
   
 }
