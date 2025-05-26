f1e137b585fbf415be7caade07ed8e9acdd2a7dd
IkvlvEgr <egor.iakovlev@go.shoreline.edu>
2025-05-25 23:46:51 -0700

    Small Syntax Changes

--- HTMLManager.java
+++ HTMLManager.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class HTMLManager {
    private Queue<HTMLTag> tags;
-   public HTMLManager(Queue<HTMLTag> html){
+   public HTMLManager(Queue<HTMLTag> html) {
      tags = new LinkedList<>();
      if(html == null){
        throw new IllegalArgumentException();
@@ -12,42 +12,46 @@ public class HTMLManager {
      }
   }
   
-  public Queue<HTMLTag> getTags(){
+  public Queue<HTMLTag> getTags() {
      return tags;
   }
   
-  public String toString(){
+  public String toString() {
      String str = "";
      int size = tags.size();
-     for( int i = 0; i < size; i++){
+     for(int i = 0; i < size; i++) {
       HTMLTag tag = tags.remove();
       str += tag.toString().trim();
       tags.add(tag);
      }
      return str;
   }
-  public void fixHTML(){
+  
+  public void fixHTML() {
+  
     Stack<HTMLTag> ta = new Stack<>();
      Queue<HTMLTag> qu = new LinkedList<>();
-     while(!tags.isEmpty()){
+     
+     while(!tags.isEmpty()) {
        HTMLTag cur = tags.remove();
-        if(cur.isSelfClosing()){
+        if(cur.isSelfClosing()) {
            qu.add(cur);
         }
-        else if(cur.isOpening()){
+        else if(cur.isOpening()) {
            qu.add(cur);
            ta.push(cur);
        }
-       else if(cur.isClosing()){
-         if(ta.isEmpty()){
+       else if(cur.isClosing()) {
+         if(ta.isEmpty()) {
+         
          }
          else{
             HTMLTag top = ta.peek();
-            if(top.matches(cur)){
+            if(top.matches(cur)) {
                qu.add(cur);
                ta.pop();
             }
-            else{
+            else {
              HTMLTag find = ta.pop().getMatching();
                qu.add(find);
             }
@@ -55,17 +59,14 @@ public class HTMLManager {
          }
       }
     }
-        while(!ta.isEmpty()){
-       HTMLTag remainOpe = ta.pop();
-       HTMLTag remainCLO = remainOpe.getMatching();
-       qu.add(remainCLO);
+    
+    while(!ta.isEmpty()) {
+        
+      HTMLTag remainOpe = ta.pop();
+      HTMLTag remainCLO = remainOpe.getMatching();
+      qu.add(remainCLO);
        
-       }
-       this.tags = qu;
-
-  
+    }
+    this.tags = qu;
   }
-     
-  
-  
 }
